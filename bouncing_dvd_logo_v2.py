ad083e1372193d85dea7cd0852040e411a1aa37c
Nyxvale Orenth <nyxvaleorenth@gmail.com>
2025-12-11 13:41:08 +0200

    Done

---
+++ bouncing_dvd_logo_v2.py
@@ -0,0 +1,36 @@
+import random
+import sys
+import time
+
+import bext
+
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+
+NUMBER_OF_LOGOS = 5
+PAUSE_AMOUNT = 0.2
+COLORS = ["red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+
+UP_RIGHT = "ur"
+UP_LEFT = "ul"
+DOWN_RIGHT = "dr"
+DOWN_LEFT = "dl"
+DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+
+COLOR = "color"
+X = "x"
+Y = "y"
+DIR = "direction"
+
+
+def main():
+    bext.clear()
+
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+        logos.append(
+            {
+                COLOR: random.choice(COLORS),
+                X: random.randint(1, WIDTH - 4),
+            }
+        )
